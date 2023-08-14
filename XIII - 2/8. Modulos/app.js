b72877fcdbaf120dd5fe329aeb650917e4bf2a5d
Diego Ospina <140738227+diegospina1@users.noreply.github.com>
2023-08-14 08:54:14 -0500

    Delete XIII - 2 directory

--- XIII - 2/8. Modulos/app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import {potencia as pot} from "./calculos.js";
-
-console.log(pot(2,3));
-
--- XIII - 2/8. Modulos/calculos.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const potencia = (num, potencia) =>{
-    return Math.pow(num, potencia);
-};
-
-export {potencia};
-
